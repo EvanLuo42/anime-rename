cb7585145e880bff37cfa3d21edd0b3460fd3145
Evan Luo <knacks.insects_0o@icloud.com>
2024-01-19 11:42:24 +0800

    fix: Wrong args

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,42 @@
 use std::fs;
 
 use clap::Parser;
+use regex::Regex;
 
 fn main() {
     let args = Args::parse();
-    match args.torrent_name {
-        torrent_name if torrent_name.contains("桜都字幕组") => ying_du_pattern(torrent_name, args.file_path, args.root_path),
+    match args.file_path {
+        file_path if file_path.contains("Sakurato") => ying_du_pattern(file_path),
         _ => {}
     }
 }
 
-fn ying_du_pattern(torrent_name: String, file_path: String, root_path: String) {
-    let episode = torrent_name
-        .split_ascii_whitespace()
-        .last()
-        .unwrap()
-        .replace('[', "")
-        .replace(']', " ")
-        .split_ascii_whitespace()
-        .collect::<Vec<&str>>()
-        .first()
-        .unwrap()
-        .to_string();
-    let mut directories = file_path.clone().split('/').map(|x| x.to_string()).collect::<Vec<String>>();
+fn ying_du_pattern(file_path: String) {
+    let mut directories = file_path.split('/').map(|x| x.to_string()).collect::<Vec<String>>();
     let file_name = directories.pop().unwrap();
+    let extension = file_name.split('.').last().unwrap().to_string();
     let season = directories.pop().unwrap().split_ascii_whitespace().last().unwrap().to_string();
     let anime_name = directories.pop().unwrap();
-    let extension = file_name.split('.').last().unwrap().to_string();
-    fs::rename(file_path, format!("{}/{} - s{}e{}.{}", root_path, anime_name, season, episode, extension)).unwrap();
+    let regex = Regex::new(r"\[(.*?)\]").unwrap();
+    let mut matches = regex.captures_iter(&file_name);
+    let _fansub = matches.next().unwrap().get(1).unwrap().as_str();
+    let episode = matches.next().unwrap().get(1).unwrap().as_str();
+    let new_file_name = format!("{} - s{}e{}.{}", anime_name, season, episode, extension);
+    fs::rename(file_path, format!("{}/{}", directories.clone().remove(directories.len() - 1), new_file_name)).unwrap();
+}
+
+#[cfg(test)]
+mod tests {
+    use regex::Regex;
+
+    #[test]
+    fn test_regex() {
+        let regex = Regex::new(r"\[(.*?)\]").unwrap();
+        let mut matches = regex.captures_iter("[Sakurato] Sousou no Frieren [01][HEVC-10bit 1080p AAC][CHS&CHT].mkv");
+        let _fansub = matches.next().unwrap().get(1).unwrap().as_str();
+        let episode = matches.next().unwrap().get(1).unwrap().as_str();
+        assert_eq!(episode, "01");
+    }
 }
 
 #[derive(Parser, Debug)]
@@ -41,11 +50,4 @@ pub struct Args {
     /// Path of the file waiting for being renamed
     #[arg(short, long)]
     file_path: String,
-
-    /// Name of the file waiting for being renamed
-    #[arg(short, long)]
-    torrent_name: String,
-
-    #[arg(short, long)]
-    root_path: String
 }
